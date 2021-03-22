44823fcb1935ca700331763590557a8ef4a1af3a
Jared Hess <nobody6.1@gmail.com>
2021-03-21 22:49:51 -0700

    Clean up some syntax

--- index.js
+++ index.js
@@ -1,54 +1,49 @@
-'use strict';
+const moment = require('moment');
+const stringify = require('csv-stringify');
+const Datastore = require('nedb');
 
-
-var moment = require('moment');
-var stringify = require('csv-stringify');
-var Datastore = require('nedb');
-var db = new Datastore();
-
+const db = new Datastore();
 
 const fs = require('fs');
 
-let rawdata = fs.readFileSync('data.json');
-let data = JSON.parse(rawdata);
-let messages = data.messages;
+const rawdata = fs.readFileSync('data.json');
+const data = JSON.parse(rawdata);
+const { messages } = data;
 let logins = {};
-let sessions = [];
-for (const i in messages){
-	const message = messages[i];
-	const content = message.content;
-	const timestamp = moment(message.timestamp);
-	console.log(content + " " + timestamp.format());
-	
-	if (content.includes(" connected ")){
-		var name = content.split(" connected ")[0];
-		logins[name] = timestamp;
-	} else if (content.includes(" disconnected ")){
-		var name = content.split(" disconnected ")[0];
-		
-		if(name in logins){
-			var start = logins[name];
-			var end = timestamp;
-			var duration = moment.duration(end.diff(start));
-			var session = {name: name, start: start, duration: duration};
-			db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id))
-			sessions.push(session);
-			delete logins[name]
-		} else {
-			console.log("Disconnect for " + name + " found with no connect. Skipping...")
-		}
-	} else if (content.includes(" stopping")){
-		for ( const name in logins){
-			var start = logins[name];
-			var end = timestamp;
-			var duration = moment.duration(end.diff(start));
-			var session = {name: name, start: start, duration: duration};
-			db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id))
-			sessions.push(session);
-			delete logins[name];
-		}
-		logins = {};
-	}
-}
+const sessions = [];
+Object.keys(messages).forEach(i => {
+  const message = messages[i];
+  const { content } = message;
+  const timestamp = moment(message.timestamp);
+  console.log(`${content} ${timestamp.format()}`);
 
+  if (content.includes(' connected ')) {
+    const name = content.split(' connected ')[0];
+    logins[name] = timestamp;
+  } else if (content.includes(' disconnected ')) {
+    const name = content.split(' disconnected ')[0];
 
+    if (name in logins) {
+      const start = logins[name];
+      const end = timestamp;
+      const duration = moment.duration(end.diff(start));
+      const session = { name, start, duration };
+      db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id));
+      sessions.push(session);
+      delete logins[name];
+    } else {
+      console.log(`Disconnect for ${name} found with no connect. Skipping...`);
+    }
+  } else if (content.includes(' stopping')) {
+    logins.forEach(name => {
+      const start = logins[name];
+      const end = timestamp;
+      const duration = moment.duration(end.diff(start));
+      const session = { name, start, duration };
+      db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id));
+      sessions.push(session);
+      delete logins[name];
+    })
+    logins = {};
+  }
+})
