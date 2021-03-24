e5811128ffc9d2894778c0e103ee96e638d0ba40
Jared Hess <nobody6.1@gmail.com>
2021-03-23 17:09:27 -0700

    Fix formatting

--- commands/playtime.js
+++ commands/playtime.js
@@ -3,17 +3,16 @@ module.exports = {
   description: 'Get Playtime',
   cooldown: 5,
   execute(message, args) {
-    let client = message.client;
-    let db = client.db;
+    const { client } = message;
+    const { db } = client;
 
     const name = args[0];
-    db.find({name: name}, (err, docs) => {
-      console.log(docs[0].duration._milliseconds)
-      console.log(typeof docs[0].duration._milliseconds)
-      ms = docs.reduce((acc, cur) => acc + cur.duration._milliseconds, 0)
-      message.channel.send(ms/(1000*60*60))
-  
-    })
+    db.find({ name }, (err, docs) => {
+      console.log(docs[0].duration._milliseconds);
+      console.log(typeof docs[0].duration._milliseconds);
+      ms = docs.reduce((acc, cur) => acc + cur.duration._milliseconds, 0);
+      message.channel.send(ms / (1000 * 60 * 60));
+    });
 
     message.channel.send('Pong.');
   },
