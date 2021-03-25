2c6636f0b79eae99a225d3b1f27826eb0b3da2d7
Jared Hess <nobody6.1@gmail.com>
2021-03-24 18:17:41 -0700

    Use mongo

--- commands/playtime.js
+++ commands/playtime.js
@@ -1,17 +1,69 @@
+const assert = require('assert');
+
 module.exports = {
   name: 'playtime',
   description: 'Get Playtime',
   cooldown: 5,
   execute(message, args) {
     const { client } = message;
-    const { db } = client;
+    const db = client.db.sessions;
 
     const name = args[0];
-    db.find({ name }, (err, docs) => {
-      console.log(docs[0].duration._milliseconds);
-      console.log(typeof docs[0].duration._milliseconds);
-      ms = docs.reduce((acc, cur) => acc + cur.duration._milliseconds, 0);
+    db.find({ name }).toArray((err, docs) => {
+      console.log(docs[0].duration);
+      console.log(typeof docs[0].duration);
+      const ms = docs.reduce((acc, cur) => acc + cur.duration, 0);
       message.channel.send(ms / (1000 * 60 * 60));
+    });
+
+    const agg = [
+      {
+        $match: {
+          name,
+        },
+      }, {
+        $project: {
+          year: {
+            $year: '$start',
+          },
+          day: {
+            $dayOfYear: '$start',
+          },
+          date: {
+            $dateToString: {
+              format: '%Y-%m-%d',
+              date: '$start',
+            },
+          },
+          duration: 1,
+        },
+      }, {
+        $group: {
+          _id: {
+            date: '$date',
+          },
+          total: {
+            $sum: {
+              $divide: [
+                '$duration', 1000 * 60 * 60,
+              ],
+            },
+          },
+        },
+      }, {
+        $sort: {
+          '_id.date': -1,
+        },
+      }, {
+        $limit: 7,
+      },
+    ];
+
+    db.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+      result.toArray((err, docs) => {
+        console.log(docs);
+      });
     });
 
     message.channel.send('Pong.');
--- index.js
+++ index.js
@@ -1,112 +1,125 @@
 require('dotenv').config();
 const fs = require('fs');
-const moment = require('moment');
-const Datastore = require('nedb');
 const Discord = require('discord.js');
-
-const db = new Datastore();
-const client = new Discord.Client();
-client.commands = new Discord.Collection();
-client.db = db;
+const { MongoClient } = require('mongodb');
+const splitargs = require('splitargs');
 
 // Env Vars
 const prefix = process.env.PREFIX;
 const token = process.env.DISCORD_TOKEN;
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017';
 
-// Setup discord command folder
-const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
-commandFiles.forEach((file) => {
-  // eslint-disable-next-line import/no-dynamic-require
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
-});
+// DB
+MongoClient.connect(mongoUrl, (err, dbclient) => {
+  const db = dbclient.db('mytestingdb');
+  const sessions = db.collection('sessions');
+  sessions.createIndex({ session_id: 1 }, { unique: true });
+  const client = new Discord.Client();
+  client.commands = new Discord.Collection();
+  client.db = { sessions };
 
-const rawdata = fs.readFileSync('data.json');
-const data = JSON.parse(rawdata);
-const { messages } = data;
-let logins = {};
-const sessions = [];
-Object.keys(messages).forEach((i) => {
-  const message = messages[i];
-  const { content } = message;
-  const timestamp = moment(message.timestamp);
-  console.log(`${content} ${timestamp.format()}`);
+  // Setup discord command folder
+  const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+  commandFiles.forEach((file) => {
+    // eslint-disable-next-line import/no-dynamic-require
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+  });
 
-  if (content.includes(' connected ')) {
-    const name = content.split(' connected ')[0];
-    logins[name] = timestamp;
-  } else if (content.includes(' disconnected ')) {
-    const name = content.split(' disconnected ')[0];
+  const rawdata = fs.readFileSync('data.json');
+  const data = JSON.parse(rawdata);
+  const { messages } = data;
+  let logins = {};
+  Object.keys(messages).forEach((i) => {
+    const message = messages[i];
+    const { content } = message;
+    const timestamp = new Date(message.timestamp);
+    console.log(`${content} ${timestamp}`);
 
-    if (name in logins) {
-      const start = logins[name];
-      const end = timestamp;
-      const duration = moment.duration(end.diff(start));
-      const session = { name, start, duration };
-      db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id));
-      sessions.push(session);
-      delete logins[name];
-    } else {
-      console.log(`Disconnect for ${name} found with no connect. Skipping...`);
+    if (content.includes(' connected ')) {
+      const { id } = message;
+      const name = content.split(' connected ')[0];
+      logins[name] = { timestamp, id };
+    } else if (content.includes(' disconnected ')) {
+      const name = content.split(' disconnected ')[0];
+
+      if (name in logins) {
+        const start = logins[name].timestamp;
+        const { id } = logins[name];
+        const end = timestamp;
+        const duration = end - start;
+        console.log('duration:', duration);
+        const session = {
+          name, session_id: id, start, end, duration,
+        };
+        sessions.insertOne(session, (err, doc) => console.log(doc));
+        delete logins[name];
+      } else {
+        console.log(`Disconnect for ${name} found with no connect. Skipping...`);
+      }
+    } else if (content.includes(' stopping')) {
+      Object.keys(logins).forEach((name) => {
+        const start = logins[name].timestamp;
+        const { id } = logins[name];
+        const end = timestamp;
+        const duration = end - start;
+        console.log('duration:', duration);
+        const session = {
+          name, session_id: id, start, end, duration,
+        };
+        sessions.insertOne(session, (err, doc) => console.log(doc));
+        delete logins[name];
+      });
+      logins = {};
     }
-  } else if (content.includes(' stopping')) {
-    Object.keys(logins).forEach((name) => {
-      const start = logins[name];
-      const end = timestamp;
-      const duration = moment.duration(end.diff(start));
-      const session = { name, start, duration };
-      db.insert(session, (err, doc) => console.log('Inserted', doc.name, 'with ID', doc._id));
-      sessions.push(session);
-      delete logins[name];
-    });
-    logins = {};
-  }
-});
+  });
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
+  client.on('ready', () => {
+    console.log('I am ready!');
+  });
 
-// Create an event listener for messages
-client.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  // Create an event listener for messages
+  client.on('message', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const commandName = args.shift().toLowerCase();
+    // const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = splitargs(message.content.slice(prefix.length).trim());
+    const commandName = args.shift().toLowerCase();
 
-  const command = client.commands.get(commandName)
-    || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+    const command = client.commands.get(commandName)
+      || client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
-  if (!command) return;
+    if (!command) return;
 
-  if (command.guildOnly && message.channel.type === 'dm') {
-    return message.reply('I can\'t execute that command inside DMs!');
-  }
-
-  if (command.permissions) {
-    const authorPerms = message.channel.permissionsFor(message.author);
-    if (!authorPerms || !authorPerms.has(command.permissions)) {
-      return message.reply('You can not do this!');
-    }
-  }
-
-  if (command.args && !args.length) {
-    let reply = `You didn't provide any arguments, ${message.author}!`;
-
-    if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+    if (command.guildOnly && message.channel.type === 'dm') {
+      return message.reply('I can\'t execute that command inside DMs!');
     }
 
-    return message.channel.send(reply);
-  }
+    if (command.permissions) {
+      const authorPerms = message.channel.permissionsFor(message.author);
+      if (!authorPerms || !authorPerms.has(command.permissions)) {
+        return message.reply('You can not do this!');
+      }
+    }
 
-  try {
-    command.execute(message, args);
-  } catch (error) {
-    console.error(error);
-    message.reply('there was an error trying to execute that command!');
-  }
+    if (command.args && !args.length) {
+      let reply = `You didn't provide any arguments, ${message.author}!`;
+
+      if (command.usage) {
+        reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+      }
+
+      return message.channel.send(reply);
+    }
+
+    try {
+      command.execute(message, args);
+    } catch (error) {
+      console.error(error);
+      message.reply('there was an error trying to execute that command!');
+    }
+  });
+
+  // Log our bot in using the token from https://discord.com/developers/applications
+  client.login(token);
 });
-
-// Log our bot in using the token from https://discord.com/developers/applications
-client.login(token);
