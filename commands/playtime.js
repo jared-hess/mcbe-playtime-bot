bdb040fe227d8d16453b87441ae2a399f94534cc
Jared Hess <nobody6.1@gmail.com>
2021-03-24 19:45:52 -0700

    Add chart to playtime command

--- commands/playtime.js
+++ commands/playtime.js
@@ -1,4 +1,6 @@
 const assert = require('assert');
+const moment = require('moment');
+const QuickChart = require('quickchart-js');
 
 module.exports = {
   name: 'playtime',
@@ -7,19 +9,19 @@ module.exports = {
   execute(message, args) {
     const { client } = message;
     const db = client.db.sessions;
-
     const name = args[0];
-    db.find({ name }).toArray((err, docs) => {
-      console.log(docs[0].duration);
-      console.log(typeof docs[0].duration);
-      const ms = docs.reduce((acc, cur) => acc + cur.duration, 0);
-      message.channel.send(ms / (1000 * 60 * 60));
-    });
+    const tempDate = new Date();
+    console.log(tempDate);
+    // tempDate.setDate(tempDate.getDate() - 7);
+    tempDate.setDate(tempDate.getDate() - 30);
+    const startDate = new Date(tempDate.toDateString());
+    console.log(startDate);
 
     const agg = [
       {
         $match: {
           name,
+          start: { $gt: startDate },
         },
       }, {
         $project: {
@@ -63,9 +65,26 @@ module.exports = {
       assert.equal(null, cmdErr);
       result.toArray((err, docs) => {
         console.log(docs);
+        docs.reverse();
+        const labels = docs.map(x => moment(x._id.date).format("ddd"));
+        const data = docs.map(x => x.total);
+        const label = "Hours"
+
+        const chart = new QuickChart();
+        chart.setConfig({
+          type: 'bar',
+          data: { labels, datasets: [{ label, data }] },
+        });
+        const chartEmbed = {
+          title: 'Playtime',
+          description: `Playtime for user  ${name}  over the last week`,
+          image: {
+            url: chart.getUrl(),
+          },
+        };
+        message.channel.send({ embed: chartEmbed });
       });
     });
 
-    message.channel.send('Pong.');
   },
 };
